3eb72eee8ac7391e6e59f65bfa6836cd53dcd9ac
moinakash <moinakashll@gmail.com>
2021-02-22 13:36:30 +0600

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main(){
-
   runApp(new SubClass());
-
-
-
-
 }
+
+
 
 class SubClass extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
 
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "This is App Title",
+      title: "This is App Title 2",
       home: Scaffold(
         appBar: AppBar(
            // title: Text("This is Appbar",style: TextStyle(color: Colors.white),),
-            title: Center(child: Text("This is Appbar",style: TextStyle(color: Colors.white),),),
+            title: Center(child: Text("This is Appbar 2",style: TextStyle(color: Colors.white),),),
             backgroundColor: Colors.black26),
 
         body: Material(
