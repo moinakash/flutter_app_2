b58b9e95e96eda0fd8a0622d87b93dee0208f478
moinakash <moinakashll@gmail.com>
2021-02-22 13:32:09 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(new SubClass());
+
+
+
+
+}
+
+class SubClass extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "This is App Title",
+      home: Scaffold(
+        appBar: AppBar(
+           // title: Text("This is Appbar",style: TextStyle(color: Colors.white),),
+            title: Center(child: Text("This is Appbar",style: TextStyle(color: Colors.white),),),
+            backgroundColor: Colors.black26),
+
+        body: Material(
+          color: Colors.black12,
+          child: Center(
+            child: Text("This is body Text",textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.black),),
+          ),
+
+        ),
+
+      ),
+
+    );
+  }
+}
